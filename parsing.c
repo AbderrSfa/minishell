bc5f91fecfcf23732f30e0e1714c896851375d03
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-06-09 19:15:07 +0100

    Working on parsing arguments

--- parsing.c
+++ parsing.c
@@ -1,6 +1,113 @@
 #include "parsing.h"
 
+size_t			ft_strlen(const char *s)
+{
+	size_t i;
+
+	i = 0;
+	if (s == NULL)
+		return (0);
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+int		get_size(char *s)
+{
+	int		i;
+	int		ret;
+
+	i = 0;
+	ret = 0;
+	while (s[i])
+	{
+		if (s[i] == ' ' || s[i] == '"' || s[i] == 39)
+			ret++;
+		i++;
+	}
+	printf("--%d--", ret);
+	return (ret);
+}
+
+char	**allocate_array(char *s, char **yeet)
+{
+	int		ret;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	ret = get_size(s);
+	yeet = malloc(sizeof(char *) * (ret + 1));
+	while (i < (ret + 1))
+		yeet[i++] = malloc(sizeof(char) * ft_strlen(s));
+	return (yeet);
+}
+
+void	simple_cmd_lexer(char *command)
+{
+	int		i;
+	int		y;
+	int		x;
+	char	**yeet;
+
+	i = 0;
+	y = 0;
+	x = 0;
+	yeet = allocate_array(command, yeet);
+	while (command[i])
+	{
+		while (command[i] == ' ')
+			i++;
+		if (command[i] && command[i] != '"' && command[i] != 39)
+		{
+			x = 0;
+			while (command[i] != ' ' && command[i] != '"' && command[i] != 39 && command[i])
+				yeet[y][x++] = command[i++];
+			y++;
+		}
+		if (command[i] == '"')
+		{
+			x = 0;
+			i++;
+			while (command[i] != '"' && command[i])
+				yeet[y][x++] = command[i++];
+			if (command[i] != '"')
+			{
+				printf("Unclosed double quote");
+				exit(EXIT_FAILURE);
+			}
+			y++;
+		}
+		if (command[i] == 39)
+		{
+			x = 0;
+			i++;
+			while (command[i] != 39 && command[i])
+				yeet[y][x++] = command[i++];
+			if (command[i] != 39)
+			{
+				printf("Unclosed single quote");
+				exit(EXIT_FAILURE);
+			}
+			y++;
+		}
+		i++;
+	}
+	printf("--%s--\n", yeet[0]);
+	printf("--%s--\n", yeet[1]);
+	printf("--%s--\n", yeet[2]);
+	printf("--%s--\n", yeet[3]);
+
+}
+
 int		main()
 {
+	t_simple_cmd	cmd;
+
+	simple_cmd_lexer("echo");
 	return (0);
 }
---
+++ parsing.h
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef	struct	s_simple_cmd
+{
+	char		**words;
+}				t_simple_cmd;
