00dd0a662de0a17a977ec18f084e600281f07059
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-06-13 13:06:14 +0100

    Added ft_putchar and ft_putstr to libft

--- libft/libft.h
+++ libft/libft.h
@@ -57,6 +57,8 @@ char				**ft_split(char const *s, char c);
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
+void				ft_putchar(char c);
+void				ft_putstr(char *s);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 t_list				*ft_lstnew(void *content);
