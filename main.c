3c0dd760d4ff42fe8798941eb8e96360e622870f
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-06-13 14:05:11 +0100

    Using simple_cmd_lexer with readline

--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 int		main()
 {
 	int		i;
+	char	*input;
 	t_simple_cmd	cmd;
 
-	simple_cmd_lexer(&cmd, "echo $SHELL '$PATH' ");
-	i = 0;
-	while (i < cmd.word_num)
-		printf("(%s)\n", cmd.words[i++]);
+	while (1)
+	{
+		input = readline("\033[0;32mminishell\033[0;0m:$ ");
+		simple_cmd_lexer(&cmd, input);
+		i = 0;
+		while (i < cmd.word_num)
+			printf("(%s)\n", cmd.words[i++]);
+	}
 	return (0);
 }
--- parsing.h
+++ parsing.h
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "libft/libft.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 
 typedef	struct	s_simple_cmd
 {
