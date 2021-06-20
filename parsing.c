b57441293098555d6d42dd39ce24a53c5b7ff472
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-06-20 17:01:36 +0100

    Made some progress in redirection parsing

--- parsing.c
+++ parsing.c
@@ -74,23 +74,58 @@ int	single_quotes(t_cmd *cmd, char *s, int i)
 	return (i);
 }
 
-void	redirections(t_cmd *new, char *s, int i)
+void	get_file(t_redirect *redirect, char *s, int i)
 {
-	while (s[i])
+	int		j;
+
+	while (s[i] == ' ')
+		i++;
+	j = i;
+	while (s[i] && s[i] != ' ' && s[i] != '>' && s[i] != '<')
 	{
-		
+		i++;
 	}
+	redirect->file = ft_substr(s, j, i);
+}
+
+t_redirect	*redirections(t_cmd *new, char *s, int i)
+{
+	t_redirect	*redirect;
+
+	if (!(redirect = (t_redirect *)malloc(sizeof(t_redirect))))
+		return (NULL);
 	if (s[i] == '>' && s[i + 1] != '>')
+	{
+		redirect->type = 'G';
+		printf("--%c--", redirect->type);
+		get_file(redirect, s, i + 1);
+	}
 	else if (s[i] == '<' && s[i + 1] != '<')
+	{
+		redirect->type = 'L';
+		printf("--%c--", redirect->type);
+		get_file(redirect, s, i + 1);
+	}
 	else if (s[i] == '>' && s[i + 1] == '>')
+	{
+		redirect->type = 'D';
+		printf("--%c--", redirect->type);
+		get_file(redirect, s, i + 2);
+	}
 	else if (s[i] == '<' && s[i + 1] == '<')
+	{
+		redirect->type = 'H';
+		printf("--%c--", redirect->type);
+		get_file(redirect, s, i + 2);
+	}
+	printf("(%s)", redirect->file);
 }
 
 void	get_args(t_cmd *new, char *s)
 {
-	int		i;
-	int		j;
-
+	int			i;
+	int			j;
+	t_redirect	*redirect;
 	i = 0;
 	j = 0;
 	while (s[i])
@@ -99,17 +134,16 @@ void	get_args(t_cmd *new, char *s)
 			i++;
 		if (s[i] == '<' || s[i] == '>')
 		{
-			redirections(new, s, i);
+			redirect = redirections(new, s, i);
 			break;
 		}
 		if (s[i] && s[i] != '"' && s[i] != 39)
 		{
 			j = 0;
 			while (s[i] != ' ' && s[i] != '"' && s[i] != 39
-				&& s[i] && s[i] != '<' && s[i]!= '>') // and redirection symbols
+				&& s[i] && s[i] != '<' && s[i]!= '>')
 				new->args[new->arg_num][j++] = s[i++];
 			new->args[new->arg_num][j] = '\0';
-			// if s[i] == a redirection symbol send it to a function that treats it
 			env_var_parser(new, new->args[new->arg_num]);
 			new->arg_num++;
 		}
