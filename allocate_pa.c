560bdf2170217083445c158fc37f94eb690d7682
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-07-08 15:44:30 +0100

    Norminette changes (Working well)

--- allocate_pa.c
+++ allocate_pa.c
@@ -6,13 +6,40 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 17:04:48 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/07/06 12:00:46 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/07/08 15:40:21 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-int		get_size(char *s)
+int	skip_space_and_redir(char *s, int i)
+{
+	while (s[i] == ' ')
+		i++;
+	while (s[i] == '>' || s[i] == '<')
+	{
+		i++;
+		while (s[i] == ' ')
+			i++;
+		while (s[i] && s[i] != ' ' && s[i] != '<' && s[i] != '>')
+			i++;
+	}
+	return (i);
+}
+
+int	check_for_quote(char c)
+{
+	int		quote;
+
+	quote = 0;
+	if ((c == '"' || c == 39) && quote == 0)
+		quote = 1;
+	else if ((c == '"' || c == 39) && quote == 1)
+		quote = 0;
+	return (quote);
+}
+
+int	get_size(char *s)
 {
 	int		i;
 	int		quote;
@@ -23,27 +50,13 @@ int		get_size(char *s)
 	quote = 0;
 	while (s[i])
 	{
-		while (s[i] == ' ')
-			i++;
-		while (s[i] == '>' || s[i] == '<')
-		{
-			i++;
-			while (s[i] == ' ')
-				i++;
-			while (s[i] && s[i] != ' ' && s[i] != '<' && s[i] != '>')
-				i++;
-		}
+		i = skip_space_and_redir(s, i);
 		if (s[i] && s[i] != ' ' && s[i] != '<' && s[i] != '>')
 		{
-			while ((s[i] && s[i] != ' ' && s[i] != '<' && s[i] != '>') || (s[i] == ' ' && quote)
+			while ((s[i] && s[i] != ' ' && s[i] != '<' && s[i] != '>')
+				|| (s[i] == ' ' && quote)
 				|| ((s[i] == '>' || s[i] == '<') && quote))
-			{
-				if ((s[i] == '"' || s[i] == 39) && quote == 0)
-					quote = 1;
-				else if ((s[i] == '"' || s[i] == 39) && quote == 1)
-					quote = 0;
-				i++;
-			}
+				quote = check_for_quote(s[i++]);
 			ret++;
 		}
 	}
@@ -63,41 +76,4 @@ void	allocate_args(char *s, t_cmd *cmd)
 	while (i < ret)
 		cmd->args[i++] = malloc(sizeof(char) * ft_strlen(s));
 	cmd->args[i] = NULL;
-}
-
-void	free_cmds(t_list *cmd)
-{
-	t_cmd		*temp;
-	t_list		*redir;
-	t_redirect	*temp2;
-	int		i;
-
-	i = 0;
-	while (cmd != NULL)
-	{
-		temp = cmd->content;
-		if (temp->args)
-		{
-			i = 0;
-			while (temp->args[i])
-				free(temp->args[i++]);
-			free(temp->args);
-		}
-		if (temp->redirect)
-		{
-			redir = temp->redirect;
-			while (redir != NULL)
-			{
-				temp2 = redir->content;
-				if (temp2->file)
-					free(temp2->file);
-				free(temp2);
-				free(redir);
-				redir = redir->next;
-			}
-		}
-		free(temp);
-		free(cmd);
-		cmd = cmd->next;
-	}
 }
---
+++ free_pa.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_pa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/08 15:40:02 by asfaihi           #+#    #+#             */
+/*   Updated: 2021/07/08 15:43:35 by asfaihi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+void	free_redirect(t_list *redir)
+{
+	t_redirect	*temp;
+
+	while (redir != NULL)
+	{
+		temp = redir->content;
+		if (temp->file)
+			free(temp->file);
+		free(temp);
+		free(redir);
+		redir = redir->next;
+	}
+}
+
+void	free_cmds(t_list *cmd)
+{
+	t_cmd		*temp;
+	t_list		*redir;
+	int			i;
+
+	i = 0;
+	while (cmd != NULL)
+	{
+		temp = cmd->content;
+		if (temp->args)
+		{
+			i = 0;
+			while (temp->args[i])
+				free(temp->args[i++]);
+			free(temp->args);
+		}
+		if (temp->redirect)
+			free_redirect(temp->redirect);
+		free(temp);
+		free(cmd);
+		cmd = cmd->next;
+	}
+}
+
+void	free_env_list(t_list *env_list)
+{
+	t_env	*temp;
+
+	while (env_list != NULL)
+	{
+		temp = env_list->content;
+		free(temp->key);
+		free(temp->value);
+		free(env_list->content);
+		free(env_list);
+		env_list = env_list->next;
+	}
+}
