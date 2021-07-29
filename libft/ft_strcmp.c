56d6ec65d1bd469beba69ba2d2b3aaccdd0afdf2
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-07-29 14:18:36 +0100

    Fixed a segfault in empty quotes

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 14:37:25 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/07/27 14:38:20 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/07/29 14:14:25 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (0);
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 		i++;
 	return (s1[i] - s2[i]);
