043753008bfb6987040a8b3a239cfec894340c5b
yabdelgh <yassine.abdou.1998@gmail.com>
2021-08-04 23:35:58 +0100

    export 99%

--- envp_ex.c
+++ envp_ex.c
@@ -12,12 +12,47 @@
 
 #include "exec.h"
 
+int envp_sort(char **tab, int size)
+{
+	int		i;
+	int		wall;
+	int		current;
+	char	*pivot;
+	char  	*tmp;
+	int		len;
+	
+	if (size < 2)
+		return (0);
+	wall = 0;
+	current = 0;
+	pivot = tab[size - 1];
+	len = ft_strlen(pivot);
+	while (current < size)
+	{
+		if (ft_strncmp(tab[current],pivot,len) <= 0)
+		{
+			if (current != wall)
+			{
+				tmp = tab[current];
+				tab[current] = tab[wall];
+				tab[wall] = tmp;
+			}
+			wall++;
+		}
+		current++;	
+	}
+	envp_sort(tab, wall - 1);
+	envp_sort(tab + wall - 1, size - wall + 1);
+}
+
 int ft_display_envp(t_list *envp)
 {
 	int i;
 	char **tab;
 
+	envp = envp->next;
 	tab = list_to_arr(envp);
+	envp_sort(tab, ft_lstsize(envp) + 1);
 	i = 0;
 	while (tab[i] != NULL)
 	{
@@ -35,8 +70,7 @@ char **list_to_arr(t_list *lst)
 		char	**tab;
 		t_env	*env;
 
-		size = ft_lstsize(lst);
-		lst = lst->next;
+		size = ft_lstsize(lst) + 1;
 		tab = malloc(size * sizeof(char*));
 		i = 0;
 		while(i < size - 1)
